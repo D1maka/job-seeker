1e66b0544e8d98f45c8b0ea4d92f02a343db0368
Dmytro Veres <veres.dima@gmail.com>
2015-05-07 18:42:50 +0300

    Package structure changed

--- job-seeker/src/test/java/demo/DemoApplicationTests.java
+++ job-seeker/src/test/java/demo/DemoApplicationTests.java
@@ -5,9 +5,10 @@ import org.junit.runner.RunWith;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import ua.kpi.jobseeker.Application;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = DemoApplication.class)
+@SpringApplicationConfiguration(classes = Application.class)
 @WebAppConfiguration
 public class DemoApplicationTests {
 
